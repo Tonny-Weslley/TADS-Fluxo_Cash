da69051d86c3c3add3eaf75ff60afe4063c1f7c8
Tonny-Weslley <tonnyweslleyss@gmail.com>
2022-08-13 16:36:17 -0300

    autenticando usuários

--- fluxo_cash/views/public_views.py
+++ fluxo_cash/views/public_views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.views import View
 
@@ -15,6 +17,15 @@ class Login(View):
             return redirect('app')
         return render(request, 'public/login.html')
 
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('app')
+        return render(request, 'public/login.html', context={'error': 'User or password invalid'})
+
 
 class Register(View):
     def get(self, request, *args, **kwargs):
@@ -23,4 +34,30 @@ class Register(View):
         return render(request, 'public/register.html')
 
     def post(self, request, *args, **kwargs):
-        ...
+        # verificando campos vazios
+        verified = True
+        errors = {}
+        for attribute in request.POST:
+            if(request.POST[attribute] == ''):
+                verified = False
+                errors[attribute] = 'empty'
+        # verifying if password and confirm password are equal
+        if(request.POST['password'] != request.POST['password-confirm']):
+            verified = False
+            errors['passwords'] = 'not_equal'
+
+        # verifying if user already exists
+        if(User.objects.filter(username=request.POST['username']).exists()):
+            verified = False
+            errors['user'] = 'exists'
+
+        if(verified):
+            user = User.objects.create_user(
+                request.POST['username'], request.POST['email'], request.POST['password'])
+            user.first_name = request.POST['first-name']
+            user.last_name = request.POST['last-name']
+            user.save()
+            return redirect('login')
+        else:
+            context = {'errors': errors}
+            return render(request, 'public/register.html', context)
